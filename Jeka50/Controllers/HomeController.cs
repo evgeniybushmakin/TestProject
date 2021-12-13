4a5dcd10a16906d5ebbf3a6ea87c70002b2c4c73
Illia Zhykovsky <illia9842@gmail.com>
2021-12-13 20:35:59 +0300

    * Add

---
+++ Jeka50/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using Jeka50.BDModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jeka50.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly TestBdModel _bdModel;
+        
+        public HomeController(TestBdModel bdModel)
+        {
+            _bdModel = bdModel;
+        }
+        
+        [HttpGet]
+        public async Task<JsonResult> Get(int id)
+        {
+            return new JsonResult(await _bdModel.Transactions.FindAsync(id));
+        }
+
+        [HttpPost]
+        public void Post(Transactions transUpdaate)
+        {
+            
+        }
+
+        [HttpPut]
+        public void Put(Transactions transUpdaate)
+        {
+            
+        }
+        
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            
+        }
+    }
+}
